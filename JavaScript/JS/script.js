85636141b041cb471484b2dfe508166800d86037
Kasia Goj <katarzyna.goj@fundingbox.com>
2020-11-04 19:40:09 +0100

    first commit

---
+++ JavaScript/JS/script.js
@@ -0,0 +1,5 @@
+console.log("Moje imię to Kasia")
+
+// komentarz całej linii mozna wstawic wciskając 'cmnd' + '/'
+
+/* komentarz blokowy czyli kawałka tekstu, nie samej linijki: alt + szift + a */
